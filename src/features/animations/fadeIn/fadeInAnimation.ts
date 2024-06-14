d7f4017db319adc7aa6af86dc93980a84d84c9a7
fl-yanai <yanai@framelunch.jp>
2024-06-14 18:03:49 +0900

    update

--- src/features/animations/fadeIn/fadeInAnimation.ts
+++ src/features/animations/fadeIn/fadeInAnimation.ts
@@ -1,31 +1,17 @@
+import { intersectionObserver } from '@/utils/intersectionObserver'
+
 export const fadeInAnimation = () => {
-  const intersectionObserver = (
-    targets: NodeListOf<Element>,
-    callback: (entries: any, object: any) => void,
-    options?: { root: Element | null; rootMargin: number; threshold: number },
-  ) => {
-    const observer: IntersectionObserver = new IntersectionObserver(callback, {
-      root: options ? options.root : null,
-      rootMargin: options ? `${options.rootMargin}px` : '0px',
-      threshold: options ? options.threshold : 0,
-    })
-
-    targets.forEach((element: Element) => {
-      observer.observe(element)
-    })
-  }
-
   const callback = (entries: any, object: any) => {
     entries.forEach((entry: any) => {
       if (!entry.isIntersecting) return
-      entry.target.classList.add('--targetPassed')
+      entry.target.classList.add('anime__fade-targetPassed')
       object.unobserve(entry.target)
     })
   }
 
   setTimeout(() => {
     if (typeof window === 'undefined') return
-    const targets = document.querySelectorAll(`.--target`)
+    const targets = document.querySelectorAll(`.anime__fade-target`)
     if (targets.length === 0) return
     intersectionObserver(targets, callback)
   }, 10)
---
+++ src/utils/intersectionObserver.ts
@@ -0,0 +1,15 @@
+export const intersectionObserver = (
+  targets: NodeListOf<Element>,
+  callback: (entries: any, object: any) => void,
+  options?: { root?: Element | null; rootMargin?: number; threshold?: number },
+) => {
+  const observer: IntersectionObserver = new IntersectionObserver(callback, {
+    root: options ? options.root : null,
+    rootMargin: options ? `${options.rootMargin}px` : '0px',
+    threshold: options ? options.threshold : 0,
+  })
+
+  targets.forEach((element: Element) => {
+    observer.observe(element)
+  })
+}
